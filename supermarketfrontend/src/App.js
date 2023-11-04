d985718a8ed74980d7f454256238e97144850b89
ChaviruAtthanayake <86518217+ChaviruAtthanayake@users.noreply.github.com>
2023-11-04 12:28:18 +0530

    Revert "Did some changes"

--- supermarketfrontend/src/App.js
+++ supermarketfrontend/src/App.js
@@ -1,19 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import LandingPage from "./Pages/Landing Page/LandingPage";
-
-
-function App(){
-  return(
+function App() {
+  return (
     <div className="App">
-      {<BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage/>} />
-
-          </Routes>
-
-      </BrowserRouter>}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
