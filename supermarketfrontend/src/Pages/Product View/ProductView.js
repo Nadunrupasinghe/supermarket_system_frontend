29bf6d005cfce335518505f26716f93b5ebcc14a
Gimhani Gunasinghe <93367907+gimhanigunasinghe@users.noreply.github.com>
2023-11-11 10:17:05 +0530

    Added some changes

--- supermarketfrontend/src/Pages/Product View/ProductView.js
+++ supermarketfrontend/src/Pages/Product View/ProductView.js
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProductView.css';
 import Navbar from '../../Components/Navbar/Navbar';
 import ProductViewCard from '../../Components/Product View Card/ProductViewCard';
 import Strawberry from '../../Assets/Strawberry.png';
-import Vegetables from '../../Assets/Vegetables.png';
-import cardImg3 from '../../Assets/Dairy.png';
-import cardImg4 from '../../Assets/Fruits.png';
-import cardImg5 from '../../Assets/Beverages.png';
-import cardImg6 from '../../Assets/Meat&Seafood.jpg';
-import cardImg7 from '../../Assets/Household.jpg';
-import cardImg8 from '../../Assets/Bakery.png';
-import cardImg9 from '../../Assets/CookingEssentials.jpg';
-import cardImg10 from '../../Assets/BabyProducts.jpg';
 import ProductAddCard from '../../Components/ProductAddCard/ProductAddCard';
 import ManageProductCard from '../../Components/ManageProductCard/ManageProductCard';
 
 function ProductView() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the API and update the state.
+    getAllProducts();
+
+    const getAllProducts = async () => {
+      try {
+        // Replace 'apiEndpoint' with the actual URL of your API that serves product data.
+        const response = await fetch('localhost:8181/API/products');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+  }, []);
   return (
     <div className='productView'>
       <Navbar />
       <h2 className='productViewText'>Shop Now</h2>
       <div className='cardsection'>
+        {products.map((product, skucode) => (
+          <ProductAddCard
+            key={skucode}
+            // image={product.imageURL} // Replace with the actual property name from your API response
+            name={product.name}
+            price={product.price}
+          />
+        ))}
+        {/* <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Vegetables} name='Strawberry' price='450' />
+        <ProductAddCard image={Vegetables} name='Strawberry' price='450' /> */}
       </div>
 
       <div className='cardsection2'>
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
+        {/* <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Vegetables} name='Strawberry' price='450' />
-        <ManageProductCard />
+        <ManageProductCard /> */}
       </div>
     </div>
   );
