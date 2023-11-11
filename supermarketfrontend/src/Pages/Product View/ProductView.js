0d150686d998315d869c69f26184d00f4c0cc7fe
Gimhani Gunasinghe <93367907+gimhanigunasinghe@users.noreply.github.com>
2023-11-11 10:55:58 +0530

    Added some changes

--- supermarketfrontend/src/Pages/Product View/ProductView.js
+++ supermarketfrontend/src/Pages/Product View/ProductView.js
@@ -7,45 +7,45 @@ import ProductAddCard from '../../Components/ProductAddCard/ProductAddCard';
 import ManageProductCard from '../../Components/ManageProductCard/ManageProductCard';
 
 function ProductView() {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    // Fetch products from the API and update the state.
-    getAllProducts();
+  // useEffect(() => {
+  //   // Fetch products from the API and update the state.
+  //   getAllProducts();
 
-    const getAllProducts = async () => {
-      try {
-        // Replace 'apiEndpoint' with the actual URL of your API that serves product data.
-        const response = await fetch('localhost:8181/API/products');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        console.log(data);
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      }
-    };
-  }, []);
+  //   const getAllProducts = async () => {
+  //     try {
+  //       // Replace 'apiEndpoint' with the actual URL of your API that serves product data.
+  //       const response = await fetch('localhost:8181/api/product');
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok');
+  //       }
+  //       const data = await response.json();
+  //       console.log(data);
+  //       setProducts(data);
+  //     } catch (error) {
+  //       console.error('Error fetching products:', error);
+  //     }
+  //   };
+  // }, []);
   return (
     <div className='productView'>
       <Navbar />
       <h2 className='productViewText'>Shop Now</h2>
       <div className='cardsection'>
-        {products.map((product, skucode) => (
+        {/* {products.map((product, skucode) => (
           <ProductAddCard
             key={skucode}
             // image={product.imageURL} // Replace with the actual property name from your API response
             name={product.name}
             price={product.price}
           />
-        ))}
-        {/* <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
+        ))} */}
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
         <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Vegetables} name='Strawberry' price='450' /> */}
+        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
+        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
       </div>
 
       <div className='cardsection2'>
