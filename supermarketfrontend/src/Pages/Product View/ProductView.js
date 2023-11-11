d7f0e2909d680d9f585a8dbc1a938af357c6dcfa
Gimhani Gunasinghe <93367907+gimhanigunasinghe@users.noreply.github.com>
2023-11-11 16:57:05 +0530

    Added some changes

--- supermarketfrontend/src/Pages/Product View/ProductView.js
+++ supermarketfrontend/src/Pages/Product View/ProductView.js
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ProductView.css';
 import Navbar from '../../Components/Navbar/Navbar';
-import ProductViewCard from '../../Components/Product View Card/ProductViewCard';
+// import ProductViewCard from '../../Components/Product View Card/ProductViewCard';
 import Strawberry from '../../Assets/Strawberry.png';
-import Vegetables from '../../Assets/Vegetables.png';
-import cardImg3 from '../../Assets/Dairy.png';
-import cardImg4 from '../../Assets/Fruits.png';
-import cardImg5 from '../../Assets/Beverages.png';
-import cardImg6 from '../../Assets/Meat&Seafood.jpg';
-import cardImg7 from '../../Assets/Household.jpg';
-import cardImg8 from '../../Assets/Bakery.png';
-import cardImg9 from '../../Assets/CookingEssentials.jpg';
-import cardImg10 from '../../Assets/BabyProducts.jpg';
+import Fruits from '../../Assets/Fruits.png';
 import ProductAddCard from '../../Components/ProductAddCard/ProductAddCard';
-import ManageProductCard from '../../Components/ManageProductCard/ManageProductCard';
+// import ManageProductCard from '../../Components/ManageProductCard/ManageProductCard';
 
 function ProductView() {
+  const [products, setProducts] = useState([]);
+
+  const getAllProducts = React.useCallback(async () => {
+    //Functions to fetch all the products in the database
+    return fetch('http://localhost:8083/api/product', {
+      method: 'GET',
+      headers: {},
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setProducts(res);
+        //console.log(products);
+        // if (!res.products) throw new Error('No Products');
+        // setProducts(res.products);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div className='productView'>
       <Navbar />
       <h2 className='productViewText'>Shop Now</h2>
       <div className='cardsection'>
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Vegetables} name='Strawberry' price='450' />
-      </div>
-
-      <div className='cardsection2'>
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Strawberry} name='Strawberry' price='450' />
-        <ProductAddCard image={Vegetables} name='Strawberry' price='450' />
-        <ManageProductCard />
+        {products.map((product, skucode) => (
+          <ProductAddCard
+            key={skucode}
+            image={Fruits}
+            name={product.name}
+            price={product.price}
+          />
+        ))}
       </div>
     </div>
   );
